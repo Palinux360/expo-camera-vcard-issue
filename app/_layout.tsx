73f0ba6f4072c9056a39e80883b416f7bf5220a6
“Paulo-Loaiza” <palinux360@gmail.com>
2025-02-08 22:43:03 +0000

    Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="ReadCard" options={{ title: "Scan QR Code" }} />
+    </Stack>
+  );
 }
